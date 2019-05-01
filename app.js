35a3db87e52bc39f83654e089e664230330480f9
_immayurpanchal <mayur.panchal@vuclip.com>
2019-05-01 18:39:29 +0530

    First Response

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  console.log(req.method);
-  console.log(req.headers);
-  process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Node Practice</title></head>');
+    res.write('<body><h1>This is response from Node.js server</h1></body>');
+    res.end();
 });
 
 server.listen(3000);
