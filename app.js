a2bab3b12422224f9eb86e5aaf34bd024eae095e
Mayur Panchal <mayurpanchal002@gmail.com>
2019-05-14 09:18:55 +0530

    routes added

--- app.js
+++ app.js
@@ -1,37 +1,7 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Node Practice</title></head>");
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Submit</button></form></body>"
-    );
-    return res.end();
-  }
+const routes = require("./routes");
 
-  if(req.url === '/message' && req.method === 'POST'){
-      const body = [];
-      req.on('data', (chunk) => {
-          body.push(chunk); //Chunk is unreadable data in int form 
-      });
-
-      req.on('end', () => {
-          const parsedBody = Buffer.concat(body).toString(); //chunk is readable 
-          const message = parsedBody.split('=')[1];
-          fs.writeFileSync('msg.txt', message);
-      });
-      res.statusCode = 302;
-      res.setHeader('location', '/');
-      return res.end();
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>Node Practice</title></head>");
-  res.write("<body><h1>This is response from Node.js server</h1></body>");
-  res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
