7eb6c7001e01564cf9883a49012d25b91ce656ce
Mayur Panchal <mayurpanchal002@gmail.com>
2019-05-16 18:47:11 +0530

    added body parser

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
+/* eslint-disable max-len */
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 //* order matters in defining the path
-app.use('/', (req, res, next) => {
-  console.log('This always runs');
-  next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+  res.write(
+      `<div><form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form></div>`
+  );
 });
 
-app.use('/add-product', (req, res) => {
-  res.write('<h1>Add Product</h1>');
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res) => {
-  console.log('In the another middleware');
   res.write('<h1>Hello From Express</h1>');
 });
 
