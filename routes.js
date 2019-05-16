5fb00fce9dd230567733b5579c68d8cc76615388
Mayur Panchal <mayurpanchal002@gmail.com>
2019-05-16 09:19:28 +0530

    Added nodemon, eslint config and package.json

--- routes.js
+++ routes.js
@@ -33,7 +33,7 @@ const requestHandler = (req, res) => {
   res.end();
 };
 
-// module.exports = requestHandler;
+module.exports = requestHandler;
 
 // module.exports = {
 //     handler: requestHandler,
